d98afc38b3b28eb9be96bd06f99076bc4b49ebba
RamaKrishnaVeniAndey <137883949+RamaKrishnaVeniAndey@users.noreply.github.com>
2023-07-28 19:10:45 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,24 @@
+ReactDOM.render(
+    <div>
+        <h1> BIOGRAPHY OF MOTHER TERESA</h1>
+        <img src="https://images.news18.com/ibnlive/uploads/2021/09/mother-teresa-16623180794x3.jpg" width="300px" height="300px"></img>
+        <h1>Mother Teresa</h1>
+          <p>
+          Mother Teresa's given name was Anjezë Gonxhe  Bojaxhiu  Gonxhe means "rosebud" or "little flower" in Albanian. She was born on 26 August 1910 into a Kosovar Albanian family in Skopje, Ottoman Empire (now the capital of North Macedonia). She was baptised in Skopje the day after her birth. She later considered 27 August, the day she was baptised, her "true birthday".
+
+She was the youngest child of Nikollë and Dranafile Bojaxhiu (Bernai). Her father, who was involved in Albanian-community politics in Ottoman North Macedonia, died in 1919 when she was eight years old. He was born in Prizren (today in Kosovo), however, his family was from Mirdita (present-day Albania). Her mother may have been from a village near Gjakova, believed by her offspring to be Bishtazhin.
+
+According to a biography by Joan Graff Clucas, Anjezë was in her early years when she became fascinated by stories of the lives of missionaries and their service in Bengal; by age 12, she was convinced that she should commit herself to religious life. Her resolve strengthened on 15 August 1928 as she prayed at the shrine of the Black Madonna of Vitina-Letnice, where she often went on pilgrimages.
+
+Anjezë left home in 1928 at age 18 to join the Sisters of Loreto at Loreto Abbey in Rathfarnham, Ireland, to learn English with the intent of becoming a missionary; English was the language of instruction of the Sisters of Loreto in India. She saw neither her mother nor her sister again. Her family lived in Skopje until 1934, when they moved to Tirana.
+
+She arrived in India in 1929 and began her novitiate in Darjeeling, in the lower Himalayas,where she learned Bengali and taught at St. Teresa's School near her convent. She took her first religious vows on 24 May 1931. She chose to be named after Thérèse de Lisieux, the patron saint of missionaries;because a nun in the convent had already chosen that name, she opted for its Spanish spelling of Teresa.
+
+Teresa took her solemn vows on 14 May 1937 while she was a teacher at the Loreto convent school in Entally, eastern Calcutta, taking the style of 'Mother' as part of Loreto custom. She served there for nearly twenty years and was appointed its headmistress in 1944. Although Mother Teresa enjoyed teaching at the school, she was increasingly disturbed by the poverty surrounding her in Calcutta. The Bengal famine of 1943 brought misery and death to the city, and the August 1946 Direct Action Day began a period of Muslim-Hindu violence.
+
+In 1946, during a visit to Darjeeling by train, Mother Teresa felt that she heard the call of her inner conscience to serve the poor of India for Jesus. She asked for and received permission to leave the school. In 1950, she founded the Missionaries of Charity, choosing a white sari with two blue borders as the order's habit.
+            </p>  
+            
+    </div>
+    ,
+document.getElementById('content'));
